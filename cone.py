e9305042469d6f2463b1723d34d168ff8540b37d
Mrinal Bhave <mrinalbhave27@gmail.com>
2022-01-21 12:52:36 +0530

    Clean up and organize code

--- cone.py
+++ cone.py
@@ -1,21 +1,22 @@
-import random,pygame
+import pygame
+import random
+
 
 class ConeBlock:
-    def __init__(self,x=None,y=None):
+    def __init__(self, x=None, y=None):
         self.x = x
-        self.y = y 
-        self.width = 50
+        self.y = y
+        self.width = 30
         self.height = 30
-        self.img = pygame.image.load('assets/cone.png')
-        IMAGE_SMALL = pygame.transform.scale(self.img, (50, 30))
+        self.img = pygame.image.load('assets/img.png')
+        IMAGE_SMALL = pygame.transform.scale(self.img, (30, 30))
         self.img = IMAGE_SMALL
         if x is None and y is None:
             self.setPosition()
 
-
     def setPosition(self):
-        self.x = random.randrange(0, 600, 1)
-        self.y = random.randrange(0, 600, 1)
-    
-    def draw(self,win):
-        win.blit(self.img,(self.x,self.y))
+        self.x = random.randrange(0, 500, 1)
+        self.y = random.randrange(0, 500, 1)
+
+    def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
---
+++ serverold.py
@@ -0,0 +1,55 @@
+import socket
+from _thread import *
+from player import Player
+import pickle
+
+server = "192.168.1.70"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for connection, Server Started")
+
+players = [Player(0, 0, 20, 20, (255, 0, 0)), Player(100, 100, 20, 20, (0, 255, 0))]
+
+
+def threaded_client(conn, player):
+    conn.send(pickle.dumps(players[player]))
+    reply = ""
+    while True:
+        try:
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                if player == 1:
+                    reply = players[0]
+                else:
+                    reply = players[1]
+                print("Received: ", data)
+                print("Sending: ", reply)
+
+            conn.sendall(pickle.dumps(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+
+currentPlayer = 0
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
